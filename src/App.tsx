666f5d956a33f744789c546b6a9e605d29eb6202
wrogoz <wrogoz@gmail.com>
2021-04-22 09:21:37 +0200

    bb

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Test from './test'
+import Test2 from './test2'
+
 function App() {
   return (
-    <div >
-      <p>this is from branch a</p>
-      <Test/>
+    <div className="App">
+     <Test2/>
     </div>
   );
 }
---
+++ src/test2.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Test2() {
+    return (
+        <div>
+            test2 z mastera
+        </div>
+    )
+}
