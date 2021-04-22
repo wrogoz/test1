ca64fab07165f160303f8880df3401ce0d56267a
wrogoz <wrogoz@gmail.com>
2021-04-22 09:22:51 +0200

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Test from './test'
+import Test2 from './test2'
 function App() {
   return (
     <div >
       <p>this is from branch a</p>
       <Test/>
+      <Test2/>
     </div>
   );
 }
-
+g
 export default App;
